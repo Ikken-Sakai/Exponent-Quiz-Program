a291fc45039e1cb7b585e8ef3f67475d06b670ad
IkkenSakai <118234664+IkkenSakai@users.noreply.github.com>
2022-11-14 19:15:10 -0700

    Create converter.py

---
+++ converter.py
@@ -0,0 +1,32 @@
+import random
+import time
+
+n = random.randint(0, 50)
+
+correct = True
+while correct == True:
+	correct = False
+	print("What is 2 ** {}?".format(n))
+
+	r = n % 10
+	d = n // 10
+
+	answer = 2**r
+	if d == 1:
+		answer = str(answer) + 'K'
+	elif d == 2:
+		answer = str(answer) + 'M'
+	elif d == 3:
+		answer = str(answer) + 'B'
+	elif d == 0:
+		answer = str(answer) + ""
+	else:
+		answer = str(answer) + 'T'
+	print(answer)
+
+	a = input()
+	if a != answer:
+		print("Wrong, try again")
+		correct = True
+	else:
+		collect = False
